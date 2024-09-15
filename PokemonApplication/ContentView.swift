750715ca490a63b23d20fa1fbf104584ba2c2d09
Sai Aik Sei Mouk <saiseimouk.mdy@gmail.com>
2024-09-15 13:10:30 +0700

    Initial Commit

---
+++ PokemonApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonApplication
+//
+//  Created by Sei Mouk on 15/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonApplication/PokemonApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApplicationApp.swift
+//  PokemonApplication
+//
+//  Created by Sei Mouk on 15/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
